e9e15946a8ab390f7f650a540e71e2c3441fa56b
luz33c <33cluz@gmail.com>
2022-09-28 10:03:34 +0800

    feat: updatetranscoder

---
+++ example/src/renderer/examples/advanced/LocalVideoTranscoder/LocalVideoTranscoder.tsx
@@ -0,0 +1,593 @@
+import React from 'react';
+import createAgoraRtcEngine, {
+  ChannelProfileType,
+  ClientRoleType,
+  IMediaPlayer,
+  IMediaPlayerSourceObserver,
+  IRtcEngineEventHandler,
+  MediaPlayerError,
+  MediaPlayerState,
+  RtcConnection,
+  RtcStats,
+  ScreenCaptureSourceInfo,
+  ScreenCaptureSourceType,
+  TranscodingVideoStream,
+  VideoDeviceInfo,
+  VideoSourceType,
+  MediaSourceType,
+  // VideoTranscoderError,
+} from 'agora-electron-sdk';
+
+import Config from '../../../config/agora.config';
+
+import { getResourcePath } from '../../../utils';
+import {
+  BaseComponent,
+  BaseVideoComponentState,
+} from '../../../components/BaseComponent';
+import {
+  AgoraButton,
+  AgoraDivider,
+  AgoraDropdown,
+  AgoraImage,
+  AgoraText,
+  AgoraTextInput,
+} from '../../../components/ui';
+import { Card, List } from 'antd';
+import RtcSurfaceView from '../../../components/RtcSurfaceView';
+import { rgbImageBufferToBase64 } from '../../../utils/base64';
+
+interface State extends BaseVideoComponentState {
+  videoDevices?: VideoDeviceInfo[];
+  videoDeviceId?: string[];
+  sources?: ScreenCaptureSourceInfo[];
+  targetSource?: ScreenCaptureSourceInfo;
+  startScreenCapture: boolean;
+  url: string;
+  open: boolean;
+  imageUrl: string;
+  startLocalVideoTranscoder: boolean;
+  videoInputStreams: TranscodingVideoStream[];
+}
+
+export default class LocalVideoTranscoder
+  extends BaseComponent<{}, State>
+  implements IRtcEngineEventHandler, IMediaPlayerSourceObserver
+{
+  protected player?: IMediaPlayer;
+
+  protected createState(): State {
+    return {
+      appId: Config.appId,
+      enableVideo: true,
+      channelId: Config.channelId,
+      token: Config.token,
+      uid: Config.uid,
+      joinChannelSuccess: false,
+      remoteUsers: [],
+      startPreview: false,
+      videoDevices: [],
+      videoDeviceId: [],
+      sources: [],
+      targetSource: undefined,
+      startScreenCapture: false,
+      url: 'https://agora-adc-artifacts.oss-cn-beijing.aliyuncs.com/video/meta_live_mpk.mov',
+      open: false,
+      imageUrl: getResourcePath('png.png'),
+      startLocalVideoTranscoder: false,
+      videoInputStreams: [],
+    };
+  }
+
+  /**
+   * Step 1: initRtcEngine
+   */
+  protected async initRtcEngine() {
+    const { appId } = this.state;
+    if (!appId) {
+      this.error(`appId is invalid`);
+    }
+
+    this.engine = createAgoraRtcEngine();
+    this.engine.registerEventHandler(this);
+    this.engine.initialize({
+      appId,
+      // Should use ChannelProfileLiveBroadcasting on most of cases
+      channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
+    });
+
+    // Need to enable video on this case
+    // If you only call `enableAudio`, only relay the audio stream to the target channel
+    this.engine.enableVideo();
+
+    // Start preview before joinChannel
+    this.engine.startPreview();
+    this.setState({ startPreview: true });
+
+    this.enumerateDevices();
+    this.getScreenCaptureSources();
+    (window as any).engine = this.engine;
+  }
+
+  /**
+   * Step 2: joinChannel
+   */
+  protected joinChannel() {
+    const { channelId, token, uid } = this.state;
+    if (!channelId) {
+      this.error('channelId is invalid');
+      return;
+    }
+    if (uid < 0) {
+      this.error('uid is invalid');
+      return;
+    }
+    // start joining channel
+    // 1. Users can only see each other after they join the
+    // same channel successfully using the same app id.
+    // 2. If app certificate is turned on at dashboard, token is needed
+    // when joining channel. The channel name and uid used to calculate
+    // the token has to match the ones used for channel join
+    this.engine?.joinChannel(token, channelId, uid, {
+      // Make myself as the broadcaster to send stream to remote
+      clientRoleType: ClientRoleType.ClientRoleBroadcaster,
+      publishMicrophoneTrack: false,
+      publishCameraTrack: false,
+      publishTrancodedVideoTrack: true,
+    });
+  }
+
+  /**
+   * Step 3-1: enumerateDevices
+   */
+  enumerateDevices = () => {
+    const videoDevices = this.engine
+      .getVideoDeviceManager()
+      .enumerateVideoDevices();
+
+    this.setState({
+      videoDevices,
+      videoDeviceId: [videoDevices[0].deviceId],
+    });
+  };
+
+  // startCameraCapture = (deviceId: string) => {
+  //   this.engine?.startCameraCapture(this._getVideoSourceTypeCamera(deviceId), {
+  //     deviceId,
+  //   });
+  // };
+
+  // -- 报错 --
+  // stopCameraCapture = (deviceId: string) => {
+  //   this.engine?.stopCameraCapture(this._getVideoSourceTypeCamera(deviceId));
+  // };
+
+  /**
+   * Step 3-2: getScreenCaptureSources
+   */
+  getScreenCaptureSources = () => {
+    const sources = this.engine?.getScreenCaptureSources(
+      { width: 1920, height: 1080 },
+      { width: 64, height: 64 },
+      true
+    );
+    this.setState({
+      sources,
+      targetSource: sources[0],
+    });
+  };
+
+  /**
+   * Step 3-3 (Optional): startScreenCapture
+   */
+  startScreenCapture = () => {
+    const { targetSource } = this.state;
+    const isCaptureWindow =
+      targetSource.type ===
+      ScreenCaptureSourceType.ScreencapturesourcetypeWindow;
+
+    // -- 报错--
+    this.engine?.startScreenCaptureWithType(
+      VideoSourceType.VideoSourceScreenPrimary,
+      {
+        isCaptureWindow,
+        ...(isCaptureWindow
+          ? { windowId: targetSource.sourceId }
+          : { displayId: targetSource.sourceId }),
+      }
+    );
+    this.setState({ startScreenCapture: true });
+  };
+
+  /**
+   * Step 3-4 (Optional): stopScreenCapture
+   */
+  stopScreenCapture = () => {
+    // -- 报错--
+    // this.engine?.stopScreenCaptureWithType(
+    //   VideoSourceType.VideoSourceScreenPrimary
+    // );
+    this.setState({ startScreenCapture: false });
+  };
+
+  /**
+   * Step 3-3 (Optional): createMediaPlayer
+   */
+  createMediaPlayer = () => {
+    const { url } = this.state;
+
+    if (!url) {
+      this.error('url is invalid');
+    }
+
+    this.player = this.engine?.createMediaPlayer();
+    this.player?.registerPlayerSourceObserver(this);
+    this.player?.open(url, 0);
+  };
+
+  /**
+   * Step 3-4 (Optional): destroyMediaPlayer
+   */
+  destroyMediaPlayer = () => {
+    if (!this.player) {
+      return;
+    }
+
+    this.engine?.destroyMediaPlayer(this.player);
+    this.setState({ open: false });
+  };
+
+  /**
+   * Step 3-5: startLocalVideoTranscoder
+   */
+  startLocalVideoTranscoder = () => {
+    const config = this._generateLocalTranscoderConfiguration();
+    console.log('config data\n', config);
+
+    this.engine?.startLocalVideoTranscoder(config);
+    this.setState({ startLocalVideoTranscoder: true });
+  };
+
+  /**
+   * Step 3-6 (Optional): updateLocalTranscoderConfiguration
+   */
+  updateLocalTranscoderConfiguration = () => {
+    this.engine?.updateLocalTranscoderConfiguration(
+      this._generateLocalTranscoderConfiguration()
+    );
+  };
+
+  /**
+   * Step 3-7: stopLocalVideoTranscoder
+   */
+  stopLocalVideoTranscoder = () => {
+    this.engine?.stopLocalVideoTranscoder();
+    this.setState({ startLocalVideoTranscoder: false });
+  };
+
+  _getVideoSourceTypeCamera = (value: string) => {
+    const { videoDevices } = this.state;
+    return [
+      VideoSourceType.VideoSourceCameraPrimary,
+      VideoSourceType.VideoSourceCameraSecondary,
+      // -- 报错 --
+      // VideoSourceType.VideoSourceCameraThird,
+      // VideoSourceType.VideoSourceCameraFourth,
+    ][videoDevices.findIndex(({ deviceId }) => deviceId === value)];
+  };
+
+  _generateLocalTranscoderConfiguration = () => {
+    const { videoDeviceId, startScreenCapture, open, imageUrl } = this.state;
+    const max_width = 1080,
+      max_height = 720,
+      width = 300,
+      height = 300;
+
+    // console.log({videoDeviceId, startScreenCapture, imageUrl});
+    const streams: TranscodingVideoStream[] = [];
+    // -- 报错 --
+    // streams.push(
+    //   ...videoDeviceId.map((value) => {
+    //     return {
+    //       sourceType: this._getVideoSourceTypeCamera(value),
+    //     };
+    //   })
+    // );
+
+    if (startScreenCapture) {
+      // -- 报错 --
+      streams.push({
+        sourceType: MediaSourceType.PrimaryCameraSource, // VideoSourceType.VideoSourceScreenPrimary,
+      });
+    }
+    //
+
+    // sourceType: MediaSourceType.MediaPlayerSource,
+    //  imageUrl: this.getMediaPlayer().getMediaPlayerId().toString(),
+    if (open) {
+      // -- 报错 --
+      streams.push({
+        sourceType: MediaSourceType.MediaPlayerSource,
+        imageUrl: this.player.getMediaPlayerId().toString(),
+      });
+    }
+
+    if (imageUrl) {
+      const getImageType = (url) => {
+        if (url.endsWith('.png')) {
+          return VideoSourceType.VideoSourceRtcImagePng;
+        } else if (url.endsWith('.jepg') || url.endsWith('.jpg')) {
+          return VideoSourceType.VideoSourceRtcImageJpeg;
+        } else if (url.endsWith('.gif')) {
+          return VideoSourceType.VideoSourceRtcImageGif;
+        }
+      };
+
+      // -- 报错--
+      // streams.push({
+      //   sourceType: getImageType(imageUrl),
+      //   imageUrl: imageUrl,
+      // });
+    }
+
+    streams.map((value, index) => {
+      const maxNumPerRow = Math.floor(max_width / width);
+      const numOfRow = Math.floor(index / maxNumPerRow);
+      const numOfColumn = Math.floor(index % maxNumPerRow);
+      value.x = numOfColumn * width;
+      value.y = numOfRow * height;
+      value.width = width;
+      value.height = height;
+      value.zOrder = 1;
+      value.alpha = 1;
+      value.mirror = true;
+    });
+
+    return {
+      streamCount: streams.length,
+      videoInputStreams: streams,
+      videoOutputConfiguration: {
+        dimensions: { width: max_width, height: max_height },
+      },
+    };
+  };
+
+  /**
+   * Step 4: leaveChannel
+   */
+  protected leaveChannel() {
+    this.destroyMediaPlayer();
+    this.engine?.leaveChannel();
+  }
+
+  /**
+   * Step 5: releaseRtcEngine
+   */
+  protected releaseRtcEngine() {
+    this.engine?.release();
+  }
+
+  onLeaveChannel(connection: RtcConnection, stats: RtcStats) {
+    this.info('onLeaveChannel', 'connection', connection, 'stats', stats);
+    const state = this.createState();
+    delete state.videoDevices;
+    delete state.videoDeviceId;
+    this.setState(state);
+  }
+
+  onPlayerSourceStateChanged(state: MediaPlayerState, ec: MediaPlayerError) {
+    this.info('onPlayerSourceStateChanged', 'state', state, 'ec', ec);
+    console.log('播放');
+    switch (state) {
+      case MediaPlayerState.PlayerStateIdle:
+        break;
+      case MediaPlayerState.PlayerStateOpening:
+        break;
+      case MediaPlayerState.PlayerStateOpenCompleted:
+        this.setState({ open: true });
+        // Auto play on this case
+        this.player?.play();
+        break;
+      case MediaPlayerState.PlayerStatePlaying:
+        break;
+      case MediaPlayerState.PlayerStatePaused:
+        break;
+      case MediaPlayerState.PlayerStatePlaybackCompleted:
+        break;
+      case MediaPlayerState.PlayerStatePlaybackAllLoopsCompleted:
+        break;
+      case MediaPlayerState.PlayerStateStopped:
+        break;
+      case MediaPlayerState.PlayerStatePausingInternal:
+        break;
+      case MediaPlayerState.PlayerStateStoppingInternal:
+        break;
+      case MediaPlayerState.PlayerStateSeekingInternal:
+        break;
+      case MediaPlayerState.PlayerStateGettingInternal:
+        break;
+      case MediaPlayerState.PlayerStateNoneInternal:
+        break;
+      case MediaPlayerState.PlayerStateDoNothingInternal:
+        break;
+      case MediaPlayerState.PlayerStateSetTrackInternal:
+        break;
+      case MediaPlayerState.PlayerStateFailed:
+        break;
+    }
+  }
+
+  // onLocalVideoTranscoderError(
+  //   stream: TranscodingVideoStream,
+  //   error: VideoTranscoderError
+  // ) {
+  //   console.error('onLocalVideoTranscoderError', stream, error);
+  // }
+
+  protected renderVideo(uid: number, channelId?: string): React.ReactNode {
+    const { startLocalVideoTranscoder } = this.state;
+    const sourceType =
+      uid === 0
+        ? startLocalVideoTranscoder
+          ? VideoSourceType.VideoSourceTranscoded
+          : VideoSourceType.VideoSourceCamera
+        : VideoSourceType.VideoSourceRemote;
+
+    console.log({ startLocalVideoTranscoder, uid, sourceType });
+    return (
+      <List.Item>
+        <Card title={`ChannelId: ${channelId} Uid: ${uid}`}>
+          <AgoraText>Click view to mirror</AgoraText>
+          <RtcSurfaceView
+            canvas={{
+              uid,
+              sourceType,
+            }}
+            connection={{ channelId }}
+          />
+        </Card>
+      </List.Item>
+    );
+  }
+
+  protected renderUsers(): React.ReactNode {
+    const { videoDeviceId, channelId } = this.state;
+    return (
+      <>
+        {super.renderUsers()}
+        {videoDeviceId.map((value) => {
+          return (
+            <RtcSurfaceView
+              key={value}
+              canvas={{
+                uid: 0,
+                sourceType: this._getVideoSourceTypeCamera(value),
+              }}
+              connection={{ channelId }}
+            />
+          );
+        })}
+      </>
+    );
+  }
+
+  protected renderConfiguration(): React.ReactNode {
+    const {
+      videoDevices,
+      videoDeviceId,
+      sources,
+      targetSource,
+      startScreenCapture,
+      url,
+      open,
+      imageUrl,
+    } = this.state;
+    return (
+      <>
+        <AgoraDropdown
+          title={'videoDeviceId'}
+          items={videoDevices.map((value) => {
+            return {
+              value: value.deviceId,
+              label: value.deviceName,
+            };
+          })}
+          value={videoDeviceId}
+          onValueChange={(value, index) => {
+            if (videoDeviceId.indexOf(value) === -1) {
+              // this.startCameraCapture(value);
+              this.setState({
+                videoDeviceId: [...videoDeviceId, value],
+              });
+            } else {
+              // -- 报错 --
+              // this.stopCameraCapture(value);
+              this.setState({
+                videoDeviceId: videoDeviceId.filter((v) => v !== value),
+              });
+            }
+          }}
+        />
+        <AgoraDivider />
+        <AgoraDropdown
+          title={'targetSource'}
+          items={sources.map((value) => {
+            console.log('targetSource\n', value);
+            return {
+              value: value.sourceId,
+              label: value.sourceName,
+            };
+          })}
+          value={targetSource?.sourceId}
+          onValueChange={(value, index) => {
+            this.setState({ targetSource: sources[index] });
+          }}
+        />
+        {targetSource ? (
+          <AgoraImage
+            source={rgbImageBufferToBase64(targetSource.thumbImage)}
+          />
+        ) : undefined}
+        <AgoraButton
+          title={`${startScreenCapture ? 'stop' : 'start'} Screen Capture`}
+          onPress={
+            startScreenCapture
+              ? this.stopScreenCapture
+              : this.startScreenCapture
+          }
+        />
+        <AgoraDivider />
+        <AgoraTextInput
+          onChangeText={(text) => {
+            this.setState({ url: text });
+          }}
+          placeholder={'url'}
+          value={url}
+        />
+        {open ? (
+          <RtcSurfaceView
+            canvas={{
+              uid: this.player?.getMediaPlayerId(),
+              sourceType: VideoSourceType.VideoSourceMediaPlayer,
+            }}
+          />
+        ) : undefined}
+        <AgoraButton
+          title={`${open ? 'destroy' : 'create'} Media Player`}
+          onPress={open ? this.destroyMediaPlayer : this.createMediaPlayer}
+        />
+        <AgoraDivider />
+        <AgoraTextInput
+          onChangeText={(text) => {
+            this.setState({ imageUrl: text });
+          }}
+          placeholder={'imageUrl'}
+          value={imageUrl}
+        />
+      </>
+    );
+  }
+
+  protected renderAction(): React.ReactNode {
+    const { startLocalVideoTranscoder } = this.state;
+    return (
+      <>
+        <AgoraButton
+          title={`${
+            startLocalVideoTranscoder ? 'stop' : 'start'
+          } Local Video Transcoder`}
+          onPress={
+            startLocalVideoTranscoder
+              ? this.stopLocalVideoTranscoder
+              : this.startLocalVideoTranscoder
+          }
+        />
+        <AgoraButton
+          disabled={!startLocalVideoTranscoder}
+          title={`update Local Transcoder Configuration`}
+          onPress={this.updateLocalTranscoderConfiguration}
+        />
+      </>
+    );
+  }
+}
---
+++ example/src/renderer/examples/hooks/JoinChannelVideo/JoinChannelVideo.tsx
@@ -0,0 +1,305 @@
+import React, { useEffect, useState } from 'react';
+import {
+  ChannelProfileType,
+  ErrorCodeType,
+  ClientRoleType,
+  createAgoraRtcEngine,
+  RtcConnection,
+  RtcStats,
+  UserOfflineReasonType,
+} from 'agora-electron-sdk';
+
+import Config from '../../../config/agora.config';
+
+import RtcSurfaceView from '../../../components/RtcSurfaceView';
+import { Card, List } from 'antd';
+
+import {
+  AgoraButton,
+  AgoraDivider,
+  AgoraText,
+  AgoraTextInput,
+  AgoraView,
+} from '../../../components/ui';
+import AgoraStyle from '../../config/public.scss';
+
+export default function JoinChannelVideoWithAddlisten() {
+  const [appId] = useState(Config.appId);
+  const [enableVideo] = useState(true);
+  const [channelId, setChannelId] = useState(Config.channelId);
+  const [token] = useState(Config.token);
+  const [uid] = useState(Config.uid);
+  const [joinChannelSuccess, setJoinChannelSuccess] = useState(false);
+  const [engine] = useState(createAgoraRtcEngine());
+  const [remoteUsers, setRemoteUsers] = useState<number[]>([]);
+  const [startPreview, setStartPreview] = useState(false);
+
+  /**
+   * Step 1: initRtcEngine
+   */
+  const initRtcEngine = async () => {
+    if (!appId) {
+      error(`appId is invalid`);
+    }
+
+    engine.initialize({
+      appId,
+      // Should use ChannelProfileLiveBroadcasting on most of cases
+      channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
+    });
+    // Need to enable video on this case
+    // If you only call `enableAudio`, only relay the audio stream to the target channel
+    engine.enableVideo();
+
+    // Start preview before joinChannel
+    engine.startPreview();
+    setStartPreview(true);
+  };
+
+  /**
+   * Step 2: joinChannel
+   */
+  const joinChannel = () => {
+    if (!channelId) {
+      error('channelId is invalid');
+      return;
+    }
+    if (uid < 0) {
+      error('uid is invalid');
+      return;
+    }
+    // start joining channel
+    // 1. Users can only see each other after they join the
+    // same channel successfully using the same app id.
+    // 2. If app certificate is turned on at dashboard, token is needed
+    // when joining channel. The channel name and uid used to calculate
+    // the token has to match the ones used for channel join
+    engine?.joinChannel(token, channelId, uid, {
+      // Make myself as the broadcaster to send stream to remote
+      clientRoleType: ClientRoleType.ClientRoleBroadcaster,
+    });
+  };
+
+  /**
+   * Step 3: leaveChannel
+   */
+  const leaveChannel = () => {
+    engine?.leaveChannel();
+  };
+
+  /**
+   * Step 4: releaseRtcEngine
+   */
+  const releaseRtcEngine = () => {
+    engine?.release();
+  };
+
+  const _logSink = (
+    level: 'debug' | 'log' | 'info' | 'warn' | 'error',
+    message?: any,
+    ...optionalParams: any[]
+  ): string => {
+    console[level](message, optionalParams);
+    return `${optionalParams.map((v) => JSON.stringify(v))}`;
+  };
+
+  const debug = (message?: any, ...optionalParams: any[]): void => {
+    alert(`${message}: ${_logSink('debug', message, ...optionalParams)}`);
+  };
+
+  const log = (message?: any, ...optionalParams: any[]): void => {
+    _logSink('log', message, optionalParams);
+  };
+
+  const info = (message?: any, ...optionalParams: any[]): void => {
+    _logSink('info', message, optionalParams);
+  };
+
+  const warn = (message?: any, ...optionalParams: any[]): void => {
+    _logSink('warn', message, optionalParams);
+  };
+
+  const error = (message?: any, ...optionalParams: any[]): void => {
+    _logSink('error', message, optionalParams);
+  };
+
+  useEffect(() => {
+    initRtcEngine();
+
+    engine.addListener('onError', (err: ErrorCodeType, msg: string) => {
+      info('onError', 'err', err, 'msg', msg);
+    });
+
+    engine.addListener(
+      'onJoinChannelSuccess',
+      (connection: RtcConnection, elapsed: number) => {
+        info(
+          'onJoinChannelSuccess',
+          'connection',
+          connection,
+          'elapsed',
+          elapsed
+        );
+        setJoinChannelSuccess(true);
+        console.log('addListener:onJoinChannelSuccess', {
+          connection,
+          elapsed,
+        });
+      }
+    );
+
+    engine.addListener(
+      'onLeaveChannel',
+      (connection: RtcConnection, stats: RtcStats) => {
+        info('onLeaveChannel', 'connection', connection, 'stats', stats);
+        setJoinChannelSuccess(false);
+        setStartPreview(false);
+        setRemoteUsers([]);
+        console.log(
+          'addListener:onLeaveChannel',
+          'connection',
+          connection,
+          'stats',
+          stats
+        );
+      }
+    );
+
+    engine.addListener(
+      'onUserJoined',
+      (connection: RtcConnection, remoteUid: number, elapsed: number) => {
+        info(
+          'onUserJoined',
+          'connection',
+          connection,
+          'remoteUid',
+          remoteUid,
+          'elapsed',
+          elapsed
+        );
+        if (remoteUsers === undefined) return;
+        setRemoteUsers([...remoteUsers!, remoteUid]);
+      }
+    );
+
+    engine.addListener(
+      'onUserOffline',
+      (
+        connection: RtcConnection,
+        remoteUid: number,
+        reason: UserOfflineReasonType
+      ) => {
+        info(
+          'onUserOffline',
+          'connection',
+          connection,
+          'remoteUid',
+          remoteUid,
+          'reason',
+          reason
+        );
+        if (remoteUsers === undefined) return;
+        setRemoteUsers([...remoteUsers!, remoteUid]);
+      }
+    );
+
+    return () => {
+      engine.removeAllListeners('onJoinChannelSuccess');
+      engine.removeAllListeners('onLeaveChannel');
+      engine.removeAllListeners('onUserOffline');
+      engine.removeAllListeners('onUserJoined');
+      engine.removeAllListeners('onError');
+      releaseRtcEngine();
+    };
+  }, []);
+
+  const configuration = renderConfiguration();
+  return (
+    <AgoraView className={AgoraStyle.screen}>
+      <AgoraView className={AgoraStyle.content}>{renderUsers()}</AgoraView>
+      <AgoraView className={AgoraStyle.rightBar}>
+        {renderChannel()}
+        {configuration ? (
+          <>
+            <AgoraDivider>
+              The Configuration of JoinChannelVideoWithAddlisten
+            </AgoraDivider>
+            {configuration}
+          </>
+        ) : undefined}
+        <AgoraDivider />
+        {renderAction()}
+      </AgoraView>
+    </AgoraView>
+  );
+
+  function renderConfiguration() {
+    return undefined;
+  }
+
+  function renderUsers() {
+    return (
+      <>
+        {startPreview || joinChannelSuccess ? (
+          <List
+            style={{ width: '100%' }}
+            grid={{
+              gutter: 16,
+              xs: 1,
+              sm: 1,
+              md: 1,
+              lg: 1,
+              xl: 1,
+              xxl: 2,
+            }}
+            dataSource={[uid, ...remoteUsers]}
+            renderItem={renderVideo.bind(window)}
+          />
+        ) : undefined}
+      </>
+    );
+  }
+
+  function renderChannel() {
+    return (
+      <>
+        <AgoraTextInput
+          onChangeText={(text) => {
+            setChannelId(text);
+          }}
+          placeholder={`channelId`}
+          value={channelId}
+        />
+        <AgoraButton
+          title={`${joinChannelSuccess ? 'leave' : 'join'} Channel`}
+          onPress={() => {
+            joinChannelSuccess ? leaveChannel() : joinChannel();
+          }}
+        />
+      </>
+    );
+  }
+
+  function renderAction() {
+    return undefined;
+  }
+
+  function renderVideo() {
+    return (
+      <List.Item>
+        <Card title={`${uid === 0 ? 'Local' : 'Remote'} Uid: ${uid}`}>
+          <AgoraText>Click view to mirror</AgoraText>
+          {enableVideo ? (
+            <RtcSurfaceView canvas={{ uid }} connection={{ channelId }} />
+          ) : undefined}
+          <AgoraButton
+            title={`Append`}
+            onPress={() => {
+              setRemoteUsers([...remoteUsers!, uid]);
+            }}
+          />
+        </Card>
+      </List.Item>
+    );
+  }
+}
